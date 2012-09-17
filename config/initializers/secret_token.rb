d884741f286ea221788812bca4874c9febdb824d
Partouche <natanel.partouche@gmail.com>
2012-09-17 20:10:07 +0200

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TonAvenirNet::Application.config.secret_token = '4aa8bbb7edad081b2a77b230eff9bb4872891daa991e7c3bbaf74deca7e2743785c632d3d14045a7e267df97f6f3a9e1e7cb2881a869c244aa73988380134a98'
